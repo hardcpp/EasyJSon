a9ae1cf1a11e1e138dd5c580c72cbc199783c724
hardcpp <mail@hardcpp.net>
2013-04-23 03:22:54 +0200

    Added samples

---
+++ sample_wstring.cpp
@@ -0,0 +1,22 @@
+#include "Element.hpp"
+#include <iostream>
+
+void Write()
+{
+	EasyJSon::Node<std::wstring> l_Root;
+
+ 	l_Root[L"ID"] = L"hello world";
+ 	l_Root[L"Menu"][L"Item"][0][L"caption"] = L"test";
+ 	l_Root[L"Menu"][L"Item"][0][L"link"] 	= L"index.html";
+ 	l_Root[L"Menu"][L"Item"][1][L"caption"] = L"forum";
+ 	l_Root[L"Menu"][L"Item"][1][L"link"] 	= L"forum.html";
+
+ 	std::wcout << l_Root.Serialize<std::wostringstream>()  << std::endl;
+}
+
+int main()
+{
+	Write();
+	system("pause");
+	return 0;
+}
