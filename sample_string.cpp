eee0161557c806b6ee85fcfa84ea77a4cecc1f51
Merydwin <merydwin@gmail.com>
2015-01-05 11:40:12 +0100

    Global refactor

--- sample_string.cpp
+++ sample_string.cpp
@@ -1,22 +1,24 @@
-#include "Element.hpp"
+/// Copyright 2015 hardcpp
+/// https://github.com/hardcpp/EasyJSon
+#include "EasyJSon.hpp"
 #include <iostream>
 
 void Write()
 {
-	EasyJSon::Node<std::string> l_Root;
+    EasyJSon::Node<std::string> l_Root;
 
-	l_Root["ID"] = "hello world";
-	l_Root["Menu"]["Item"][0]["caption"] 	= "test";
-	l_Root["Menu"]["Item"][0]["link"] 	= "index.html";
-	l_Root["Menu"]["Item"][1]["caption"] 	= "forum";
-	l_Root["Menu"]["Item"][1]["link"] 	= "forum.html";
+    l_Root["ID"] = "hello world";
+    l_Root["Menu"]["Item"][0]["caption"]     = "test";
+    l_Root["Menu"]["Item"][0]["link"]     = "index.html";
+    l_Root["Menu"]["Item"][1]["caption"]     = "forum";
+    l_Root["Menu"]["Item"][1]["link"]     = "forum.html";
 
-	std::cout << l_Root.Serialize<std::ostringstream>()  << std::endl;
+    std::cout << l_Root.Serialize<std::ostringstream>()  << std::endl;
 }
 
 int main()
 {
-	Write();
-	system("pause");
-	return 0;
+    Write();
+    system("pause");
+    return 0;
 }
--- sample_wstring.cpp
+++ sample_wstring.cpp
@@ -1,22 +1,24 @@
-#include "Element.hpp"
+/// Copyright 2015 hardcpp
+/// https://github.com/hardcpp/EasyJSon
+#include "EasyJSon.hpp"
 #include <iostream>
 
 void Write()
 {
-	EasyJSon::Node<std::wstring> l_Root;
+    EasyJSon::Node<std::wstring> l_Root;
 
- 	l_Root[L"ID"] = L"hello world";
- 	l_Root[L"Menu"][L"Item"][0][L"caption"] = L"test";
- 	l_Root[L"Menu"][L"Item"][0][L"link"] 	= L"index.html";
- 	l_Root[L"Menu"][L"Item"][1][L"caption"] = L"forum";
- 	l_Root[L"Menu"][L"Item"][1][L"link"] 	= L"forum.html";
+     l_Root[L"ID"] = L"hello world";
+     l_Root[L"Menu"][L"Item"][0][L"caption"] = L"test";
+     l_Root[L"Menu"][L"Item"][0][L"link"]     = L"index.html";
+     l_Root[L"Menu"][L"Item"][1][L"caption"] = L"forum";
+     l_Root[L"Menu"][L"Item"][1][L"link"]     = L"forum.html";
 
- 	std::wcout << l_Root.Serialize<std::wostringstream>()  << std::endl;
+     std::wcout << l_Root.Serialize<std::wostringstream>()  << std::endl;
 }
 
 int main()
 {
-	Write();
-	system("pause");
-	return 0;
+    Write();
+    system("pause");
+    return 0;
 }
